9548f5f1f2ccd1fcdfab68139b7a552cdd16ffd6
alexUd01 <alexanderikpeama@gmail.com>
2023-06-26 18:24:52 +0100

    task 8 and 9

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: arguments[0],
+    gdp: arguments[1],
+    capita: arguments[2],
+  };
+
+  return budget;
+}
+// semiDONE
