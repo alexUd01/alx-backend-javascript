86b48921601965209a68a99c5aebdb29ca6d37f0
alexUd01 <alexanderikpeama@gmail.com>
2023-06-26 20:27:32 +0100

    task 8 and 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,12 @@
+export default function createReportObject(employeesList) {
+  function myFunc (anObject) {
+    return Object.keys(anObject).length;
+  }
+
+  const report = {
+    allEmployees: employeesList,
+    getNumberOfDepartments: myFunc
+  };
+
+  return report;
+}
