e09ba27b0730f4d967fc300d4938b3909c9bbcbc
alexUd01 <alexanderikpeama@gmail.com>
2023-06-26 18:26:10 +0100

    install_jest_babel_eslint.sh

---
+++ install_jest_babel_eslint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Install Jest, Babel, and ESLint
+# in your project directory:
+
+# Install Jest using:
+sudo npm install --save-dev jest
+
+# Install Babel using:
+sudo npm install --save-dev babel-jest @babel/core @babel/preset-env
+
+# Install ESLint using:
+sudo npm install --save-dev eslint
+
+# Then Finally
+sudo npm install # to install all necessary project dependencies.
