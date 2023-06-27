6728bf9e38b03f52fcf3728abbc87b27ee8a6608
alexUd01 <alexanderikpeama@gmail.com>
2023-06-27 20:23:33 +0100

    Project init, task 0 and task 1

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': 'off', // :) Wow. But the checker refused to use this :(
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
