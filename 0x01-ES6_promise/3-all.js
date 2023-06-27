ed2307811d79e7b96b2fd7df977c8eb0c25d788a
alexUd01 <alexanderikpeama@gmail.com>
2023-06-27 23:31:38 +0100

    made corrections to 3-all.js; corrected  statement and also refactored code fragments

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((result) => {
-      const s = `${result[0].body} ${result[1].firstName} ${result[1].lastName}`;
-      console.log(s);
+    .then((r) => {
+      console.log(`${r[0].body} ${r[1].firstName} ${r[1].lastName}`);
     })
-    .catch(new Error('Signup system offline'));
+    .catch(() => console.log('Signup system offline'));
 }
