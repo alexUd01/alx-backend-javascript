d0ed85370d7a435472523ac97be789b4e1db31d1
alexUd01 <alexanderikpeama@gmail.com>
2023-06-29 23:27:41 +0100

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft; // eslint-disable-line no-underscore-dangle
+    // Check if `this` inherited from class Building && if it has evacuationWarningMessage method
+    if (Object.getPrototypeOf(this) instanceof Building
+        && !this.hasOwnProperty('evacuationWarningMessage')) { // eslint-disable-line no-prototype-builtins
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft; // eslint-disable-line no-underscore-dangle
+  }
+}
