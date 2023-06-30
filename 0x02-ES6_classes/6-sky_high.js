0648a94f60372905549a126c34d0b6b56901ba91
alexUd01 <alexanderikpeama@gmail.com>
2023-06-30 03:22:24 +0100

    task 5

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,20 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  evacuationWarningMessage;
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors; // eslint-disable-line no-underscore-dangle
-  }
-
-  get sqft() {
-    return this._sqft; // eslint-disable-line no-underscore-dangle
+    this.floors = floors; // eslint-disable-line no-underscore-dangle
   }
 
   get floors() {
     return this._floors; // eslint-disable-line no-underscore-dangle
   }
 
+  set floors(value) {
+    this._floors = value; // eslint-disable-line no-underscore-dangle
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`; // eslint-disable-line no-underscore-dangle
+    return `Evacuate slowly the ${this.floors} floors`; // eslint-disable-line no-underscore-dangle
   }
 }
