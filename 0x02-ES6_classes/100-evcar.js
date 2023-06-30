b030837e80077f00f25e5a2c5f0b661269d23a69
alexUd01 <alexanderikpeama@gmail.com>
2023-06-30 03:58:43 +0100

    task 100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -2,23 +2,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this.range = range;
-  }
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
-  }
-}
-/* eslint-disable no-underscore-dangle */
+	cloneCar() {
+		return new Car();
+	}
+}/* eslint-disable no-underscore-dangle */
