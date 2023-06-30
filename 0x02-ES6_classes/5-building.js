012ee11015a57786aef672aeaae1ea9b687ed181
alexUd01 <alexanderikpeama@gmail.com>
2023-06-30 03:16:13 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,12 @@
-/* eslint-disable no-underscore-dangle */
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (!this.hasOwnProperty('evacuationWarningMessage')) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
@@ -14,14 +17,4 @@ export default class Building {
   set sqft(value) {
     this._sqft = value;
   }
-
-  evacuationWarningMessage() {
-    return ` The ${this._sqft} WARNING!!!`;
-  }
 }
-
-// Check if `this` inherited from class Building && if it has evacuationWarningMessage method
-// if (Object.getPrototypeOf(this) instanceof Building
-//     && !this.hasOwnProperty('evacuationWarningMessage')) {}
-
-/* eslint-disable no-underscore-dangle */
