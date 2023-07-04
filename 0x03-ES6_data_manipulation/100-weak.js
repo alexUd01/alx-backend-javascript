f117e767f32aea976fb2c1e5aa017abc2ac9b4ea
alexUd01 <alexanderikpeama@gmail.com>
2023-07-05 00:04:54 +0100

    corrected task 100

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,12 +1,12 @@
 export const weakMap = new WeakMap();
 
-export function queryAPI(arg) {
-  if (weakMap.get(arg) === undefined) {
-    weakMap.set(arg, 1);
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
     return;
   }
-  if (weakMap.get(arg) >= 5) {
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-  weakMap.set(arg, weakMap.get(arg) + 1);
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
 }
